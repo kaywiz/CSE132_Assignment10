c7ce12115d97cfb724f41efeb4b9c39796cd351c
k.wisneski@ACCOUNTS.AD.WUSTL.EDU <k.wisneski@ACCOUNTS.AD.WUSTL.EDU@5079ff04-d25d-4c85-8423-372005a2952e>
2016-04-14 03:02:40 +0000

    finished lab 10

--- cse132_sp16/java/assignment10/health.java
+++ cse132_sp16/java/assignment10/health.java
@@ -12,35 +12,28 @@ import java.util.ArrayList;
 
 import studio4.SerialComm;
 import studio4.SerialTestInput;
-
-public class CountStepsHealth {
-
+public class health {
 
 	public static void main(String[] args) {
 		// FIXME Auto-generated method stub
-		//String csvFile = "/Users/kaywiz/Documents/workspace/CSE132-Wisneski-437588/java/Steps.csv";
 		BufferedReader br = null;
-		BufferedWriter bw = null;
 		String line = "";
 		String csvSplitBy = ",";
-		int counter = 0;
-		
+
 		try {
 			SerialComm s = new SerialComm();
 			s.connect("/dev/cu.usbserial-DN00MZW8");
 			InputStream in = s.getInputStream();
-			OutputStream out = s.getOutputStream();		
-			bw = new BufferedWriter(new OutputStreamWriter(out));
 			br = new BufferedReader(new InputStreamReader(in, "US-ASCII"));	
-			
+			String[] initial = new String[2];
 			ArrayList<Double> dataX = new ArrayList<Double>();
 			int i=0;
 
 			while ((line = br.readLine()) != null) {
-				String[] initial = new String[3];
+				//System.out.println(br.readLine());
 				initial = line.split(csvSplitBy);
-				//System.out.println("X: " + initial[0] + ", Y: "+ initial[1] + " , Z: " + initial[2]);
-				dataX.add(Double.parseDouble(initial[2]));
+				System.out.println("Steps: " + initial[0] + ", BPM: "+ initial[1]);
+				/*dataX.add(Double.parseDouble(initial[2]));
 				//System.out.println("X: " + data[i][0] + ", Y: "+ data[i][1] + " , Z: " + data[i][2]);
 				if(i>1){
 					if(dataX.get(i) == 2){
@@ -51,17 +44,18 @@ public class CountStepsHealth {
 						counter++;
 						String str = counter + " steps";
 						System.out.println(str);
-						//bw.write(str);
+						bw.write(str);
 					}
 					else{
 						//System.out.println("Z: " + dataX.get(i) + ", is NOT a step");
 						String str = counter + " steps";
 						System.out.println(str);
-						//bw.write(counter + "wut");
+						bw.write(counter + "wut");
 					}
 				}
-				++i;
-			}	
+				++i;*/
+
+			}
 		}
 		catch (FileNotFoundException e){
 			e.printStackTrace();
@@ -73,8 +67,12 @@ public class CountStepsHealth {
 			e.printStackTrace();
 		}
 	}
+
+
+
+
+
+
 }
-
-
 
 
